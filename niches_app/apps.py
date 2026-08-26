bdfa939caec8eda163975d2f82878c29efc3f6b1
tamuno <tammytat19@gmail.com>
2026-08-26 05:38:13 -0700

    Fix settings module name in wsgi.py

--- niches_app/apps.py
+++ niches_app/apps.py
@@ -1,5 +1,21 @@
+import os
 from django.apps import AppConfig
 
 
 class NichConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'niches_app'
+
+    def ready(self):
+        # Auto-create superuser on startup using environment variables
+        try:
+            from django.contrib.auth.models import User
+            username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+            email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@nichesandhues.com")
+            password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "SuperSecurePassword123")
+
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username=username, email=email, password=password)
+        except Exception:
+            # Passes silently if database tables aren't migrated yet during build time
+            pass
--- niches_project/settings.py
+++ niches_project/settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
 
-    "niches_app",
+    "niches_app.apps.NichConfig",
 ]
 
 
